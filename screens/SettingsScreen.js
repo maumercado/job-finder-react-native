23bb0d947c61be11443187bf29d045d2b0420c85
Mauricio Mercado <mau@maumercado.com>
2018-02-01 19:34:36 -0800

    Initial Commit

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class SettingsScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>SettingsScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default SettingsScreen;
