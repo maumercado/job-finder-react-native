0dae252c96ba883db2fe4f3ad326b5916b02d70b
Mauricio Mercado <mau@maumercado.com>
2018-02-01 20:16:22 -0800

    Add navigation to settings via navigationObjects

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
 
 class ReviewScreen extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: "Review Jobs",
+        headerRight: <Button title="Settings" onPress={() => navigation.navigate("settings")} />
+    });
+
     render() {
         return (
             <View>
