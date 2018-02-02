25c4213c35c7bb2c63290219aa6161d8fa0d7dff
Mauricio Mercado <mau@maumercado.com>
2018-02-01 20:19:17 -0800

    Styled Settings button a bit more for iOS

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -5,7 +5,14 @@ import { Button } from "react-native-elements";
 class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
         title: "Review Jobs",
-        headerRight: <Button title="Settings" onPress={() => navigation.navigate("settings")} />
+        headerRight:
+            <Button
+                title="Settings"
+                onPress={() => navigation.navigate("settings")}
+                backgroundColor="rgba(0,0,0,0)"
+                color="rgba(0,122,255,1)"
+            />
+
     });
 
     render() {
