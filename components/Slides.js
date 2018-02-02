15d5b76794a51ef6990fe1a063aba309d0c79196
Mauricio Mercado <mau@maumercado.com>
2018-02-01 20:31:24 -0800

    Created Slides component to use with the welcome screen to show welcome slides

---
+++ components/Slides.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Text, View, ScrollView } from "react-native";
+
+class Slides extends Component {
+    renderSlides() {
+        return this.props.data.map(slide => {
+            return (
+                <View key={slide.text} style={styles.slideStyle}>
+                    <Text style={styles.slideText}>{slide.text}</Text>
+                </View>
+            );
+        });
+    }
+    render() {
+        return (
+            <ScrollView horizontal style={{ flex: 1 }}>
+                {this.renderSlides()}
+            </ScrollView>
+        );
+    }
+}
+
+const styles = {
+    slideStyle: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    slideText: {
+        fontSize: 30
+    }
+};
+
+export default Slides;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+
+import Slides from "../components/Slides";
+
+const SLIDE_DATA = [
+    { text: "Welcome to Job Finder" },
+    { text: "Set your location, then swipe away" }
+];
 
 class WelcomeScreen extends Component {
     render() {
-        return (
-            <View>
-                <Text>WelcomeScreen</Text>
-            </View>
-        );
+        return <Slides data={SLIDE_DATA} />;
     }
 }
 
