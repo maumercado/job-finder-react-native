4a32a7ea1ce4145d6e3e27e3c39bb545e66a1a7a
Mauricio Mercado <mau@maumercado.com>
2018-02-02 22:07:57 -0800

    Add logic so that if the user has already log in no need to show welcome screen, if it has show the welcome screen but before loading the token, show the loading screen from expo package

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { AsyncStorage } from "react-native";
+import _ from "lodash";
+import { AppLoading } from "expo";
 
 import Slides from "../components/Slides";
 
@@ -8,11 +11,25 @@ const SLIDE_DATA = [
 ];
 
 class WelcomeScreen extends Component {
+    state = { token: null };
+
+    async componentWillMount() {
+        const token = await AsyncStorage.getItem("fb_token");
+        if (token) {
+            this.props.navigation.navigate("map");
+        } else {
+            this.setState({ token: false });
+        }
+    }
+
     onSlidesComplete = () => {
         this.props.navigation.navigate("auth");
     };
 
     render() {
+        if (_.isNull(this.state.token)) {
+            return <AppLoading />;
+        }
         return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
     }
 }
