fee6a0bbef8fae55d14391c0bffd0f211858c4b7
Mauricio Mercado <mau@maumercado.com>
2018-02-05 21:15:43 -0800

    Add mapview from expo to show a real map instead of the text MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, ActivityIndicator } from "react-native";
+import { MapView } from "expo";
 
 class MapScreen extends Component {
+    state = {
+        mapLoaded: false,
+        region: { longitude: -122, latitude: 37, longitudeDelta: 0.04, latitudeDelta: 0.09 }
+    };
+
+    componentDidMount() {
+        this.setState({ mapLoaded: true });
+    }
+
     render() {
+        if (!this.state.mapLoaded) {
+            return (
+                <View style={{ flex: 1, justifyContent: "center" }}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
+        }
         return (
-            <View>
-                <Text>MapScreen</Text>
+            <View style={{ flex: 1 }}>
+                <MapView style={{ flex: 1 }} region={this.state.region} />
             </View>
         );
     }
