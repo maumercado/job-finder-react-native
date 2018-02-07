fa61f470734a797b15db41eccbf5d61540c7ea34
Mauricio Mercado <mau@maumercado.com>
2018-02-06 20:44:48 -0800

    Add functionality to MapScreen component through redux-react connect middleware

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { View, ActivityIndicator } from "react-native";
+import { Button } from "react-native-elements";
 import { MapView } from "expo";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class MapScreen extends Component {
     state = {
@@ -14,6 +17,10 @@ class MapScreen extends Component {
 
     onRegionChangeComplete = region => {
         this.setState({ region });
+    };
+
+    onButtonPress = () => {
+        this.props.fetchJobs(this.state.region);
     };
 
     render() {
@@ -31,9 +38,27 @@ class MapScreen extends Component {
                     region={this.state.region}
                     onRegionChangeComplete={this.onRegionChangeComplete}
                 />
+                <View style={styles.buttonContainer}>
+                    <Button
+                        large
+                        title="Search this area"
+                        backgroundColor="#009688"
+                        icon={{ name: "search" }}
+                        onPress={this.onButtonPress}
+                    />
+                </View>
             </View>
         );
     }
 }
 
-export default MapScreen;
+const styles = {
+    buttonContainer: {
+        position: "absolute",
+        bottom: 20,
+        left: 0,
+        right: 0
+    }
+};
+
+export default connect(null, actions)(MapScreen);
