6d17f47bf1798b5d9cbfd427bd09469b22371aa1
Mauricio Mercado <mau@maumercado.com>
2018-02-06 22:10:13 -0800

    Add functionality to show location and jobs as decks

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
+import { connect } from "react-redux";
+import { MapView } from "expo";
+import { Card } from "react-native-elements";
+
+import Swipe from "../components/Swipe";
 
 class DeckScreen extends Component {
+    renderCard(job) {
+        const initialRegion = {
+            longitude: job.longitude,
+            latitude: job.latitude,
+            latitudeDelta: 0.045,
+            longitudeDelta: 0.02
+        };
+
+        return (
+            <Card title={job.jobtitle} containerStyle={{ height: 500 }}>
+                <View style={{ height: 300 }}>
+                    <MapView
+                        scrollEnabled={false}
+                        style={{ flex: 1 }}
+                        cacheEnabled={Platform.OS === "android"}
+                        initialRegion={initialRegion}
+                    />
+                </View>
+                <View style={styles.detailWrapper}>
+                    <Text>{job.company}</Text>
+                    <Text>{job.formattedRelativeTime}</Text>
+                </View>
+                <Text>{job.snippet.replace(/<\/*b>/g, "")}</Text>
+            </Card>
+        );
+    }
+
+    renderNoMoreCards() {
+        return <Card title="No more jobs" />;
+    }
+
     render() {
         return (
-            <View>
-                <Text>DeckScreen</Text>
+            <View style={{ marginTop: 10 }}>
+                <Swipe
+                    data={this.props.jobs}
+                    renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
+                    keyProp="jobkey"
+                />
             </View>
         );
     }
 }
 
-export default DeckScreen;
+const styles = {
+    detailWrapper: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginBottom: 10
+    }
+};
+
+const mapStateToProps = ({ jobs }) => {
+    return { jobs: jobs.results };
+};
+
+export default connect(mapStateToProps)(DeckScreen);
