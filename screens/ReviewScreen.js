7473aa4916e398ee4c96adac1dde512dc6e731b7
Mauricio Mercado <mau@maumercado.com>
2018-02-07 18:53:20 -0800

    Add initial review screen card details

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { Button } from "react-native-elements";
+import { ScrollView, View, Text } from "react-native";
+import { Button, Card } from "react-native-elements";
+import { connect } from "react-redux";
 
 class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
@@ -15,13 +16,39 @@ class ReviewScreen extends Component {
 
     });
 
+    renderLikedJobs() {
+        return this.props.likedJobs.map(job => {
+            return (
+                <Card key={job.jobkey}>
+                    <View style={{ height: 200 }}>
+                        <View style={styles.detailWrapper}>
+                            <Text style={styles.italics}>{job.company}</Text>
+                            <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+                        </View>
+                    </View>
+                </Card>
+            );
+        });
+    }
+
     render() {
-        return (
-            <View>
-                <Text>ReviewScreen</Text>
-            </View>
-        );
+        return <ScrollView />;
     }
 }
 
-export default ReviewScreen;
+const styles = {
+    detailWrapper: {
+        marginBottom: 10,
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    italics: {
+        fontStyle: "italic"
+    }
+};
+
+const mapStateToProps = ({ likedJobs }) => {
+    return { likedJobs };
+};
+
+export default connect(mapStateToProps)(ReviewScreen);
