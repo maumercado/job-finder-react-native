975dae2cdf8583cd84148ef4c4c54a69fae94916
Mauricio Mercado <mau@maumercado.com>
2018-02-07 19:33:41 -0800

    Add tabbar for MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import { View, ActivityIndicator } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, Icon } from "react-native-elements";
 import { MapView } from "expo";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
 class MapScreen extends Component {
+    static navigationOptions = {
+        title: "Map",
+        tabBarIcon: ({ tintColor }) => {
+            return <Icon name="my-location" size={30} color={tintColor} />;
+        }
+    };
+
     state = {
         mapLoaded: false,
         region: { longitude: -122, latitude: 37, longitudeDelta: 0.04, latitudeDelta: 0.09 }
