5dbfbb235000a4df71f23a1bd324af4adb6550b8
Mauricio Mercado <mau@maumercado.com>
2018-02-07 19:14:22 -0800

    Adding map view to review screen and some extra styling

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { ScrollView, View, Text, Linking } from "react-native";
+import { ScrollView, View, Text, Linking, Platform } from "react-native";
 import { Button, Card } from "react-native-elements";
 import { connect } from "react-redux";
+import { MapView } from "expo";
 
 class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
@@ -18,10 +19,23 @@ class ReviewScreen extends Component {
 
     renderLikedJobs() {
         return this.props.likedJobs.map(job => {
-            const { company, formattedRelativeTime, url, jobkey } = job;
+            const { company, formattedRelativeTime, url, jobkey, jobtitle } = job;
+            const initialRegion = {
+                latitude: job.latitude,
+                longitude: job.longitude,
+                latitudeDelta: 0.045,
+                longitudeDelta: 0.02
+            };
+
             return (
-                <Card key={jobkey}>
+                <Card key={jobkey} title={jobtitle}>
                     <View style={{ height: 200 }}>
+                        <MapView
+                            scrollEnabled={false}
+                            style={{ flex: 1 }}
+                            cacheEnabled={Platform.OS === "android"}
+                            initialRegion={initialRegion}
+                        />
                         <View style={styles.detailWrapper}>
                             <Text style={styles.italics}>{company}</Text>
                             <Text style={styles.italics}>{formattedRelativeTime}</Text>
@@ -45,6 +59,7 @@ class ReviewScreen extends Component {
 const styles = {
     detailWrapper: {
         marginBottom: 10,
+        marginTop: 10,
         flexDirection: "row",
         justifyContent: "space-around"
     },
